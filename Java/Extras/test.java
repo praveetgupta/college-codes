50e1d2baf2c89e361a3977a1063cb6a41c851d20
sammedsagare <sammedsagare@gmail.com>
2023-01-25 15:16:10 +0530

    added java

---
+++ Java/Extras/test.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class test{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Hello world");
+        
+        System.out.println("Enter a number: ");
+        int num = input.nextInt();
+        System.out.println("The number you entered is: " +num);
+    }
+}
+
