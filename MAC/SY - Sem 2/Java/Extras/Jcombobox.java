3c51d7ee9aa0066e34f71977a6a7810fba468ad7
Praveet Gupta <praveetgupta@gmail.com>
2023-04-26 12:43:16 +0530

    added java combobox code

---
+++ MAC/SY - Sem 2/Java/Extras/Jcombobox.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+
+public class Jcombobox {
+
+    JFrame f;
+
+    Jcombobox() {
+
+        f = new JFrame("ComboBox Example");
+
+        String country[] = { "India", "Aus", "U.S.A", "England", "Newzealand" };
+
+        JComboBox cb = new JComboBox(country);
+
+        cb.setBounds(50, 50, 90, 20);
+
+        f.add(cb);
+
+        f.setLayout(null);
+
+        f.setSize(400, 300);
+
+        f.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+
+        new Jcombobox();
+
+    }
+
+}
