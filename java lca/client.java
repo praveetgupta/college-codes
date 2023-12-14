f49a7507ad1c5e6f8455caf90a8cd622f35f6ca9
Praveet Gupta <praveetgupta@gmail.com>
2023-12-14 09:12:12 +0530

    Deleted AJP Files

--- java lca/client.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-import java.net.*;
-
-public class client{
-    public static void main(String[] args) throws IOException{
-        System.out.println("Client Started");
-        Socket s = new Socket("localhost",8900);
-    }
-}
